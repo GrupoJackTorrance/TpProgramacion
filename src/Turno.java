52c33c8a1418d285fd1a4de63ca5622765fd175e
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2019-09-26 17:45:11 -0300

    Update Turno.java

--- src/Turno.java
+++ src/Turno.java
@@ -1,4 +1,5 @@
 
+import java.util.List;
 import java.util.Timer;
 
 
@@ -6,34 +7,35 @@ import java.util.Timer;
 public class Turno{
 		int numeroTurno;
 		int delay;
-		int cantTurnos;
 
-		public Turno(int numeroTurno, int delay, int cantTurnos) {
+		public Turno(int numeroTurno, int delay) {
 			super();
 			this.numeroTurno = numeroTurno;
 			this.delay = delay;//Tiempo de turno
-			this.cantTurnos = cantTurnos;
 		}
 
 
-		public int turno (int turno, int n) {
-			 
-	        switch (turno) {
+		public int turno (int turno, Jugador jugador,Tablero tablero) {
+	        
+			switch (turno) {
 	            case 1: 
 	            case 2: 
 	            case 3:
 	            case 4:
 	            	System.out.println("Empieza turno");
 	            	System.out.println("Juega jugador: "+turno);
+	            	int cantidad=jugador.tirarDado();
+	            	tablero.avanzarJugador(jugador, cantidad);
+	            	//luego de tirar el dado y avanzar en casillero
 	            	Contador contador=new Contador(turno);//cronometro de turno
 	            	Timer timer = new Timer();
-	   		     	timer.schedule(contador, 0, 1000);
+	   		     	timer.schedule(contador, 0, 1000);//Inicio cronometro
 	   		     	/*
 	   		     	//Lograr esta parte como una tarea en segundo plano
 	   		     	//la cual comprueba el contador para saber cuando termina turno
 	            	*/
 	            	while(contador.getSeconds()>=0) {//comprueba contador
-	            		System.out.println("acaaa");
+	            		System.out.println("Comprueba");
 	            	}
 	            	
 	            	if(contador.getSeconds()<0) {//contador en 0
@@ -44,30 +46,22 @@ public class Turno{
 	            	
 	        }
 	        
-	        n++;
 	        if(turno==5) {
             	System.out.println("termino partida");
                 System.out.println("Temporizador apagado.");
-            	n=0;
             	turno=1;
             }
 	        return turno;
 	  }
 	
 	
-     public Turno(int numeroTurno, int cantTurnos) {
-    	this.cantTurnos=cantTurnos;
+     public Turno(int numeroTurno) {
 		this.numeroTurno = numeroTurno;
 		this.delay = 60;
 	}
      
- 	public int getCantTurnos() {
-		return cantTurnos;
-	}
-
-	public void setCantTurnos(int cantTurnos) {
-		this.cantTurnos = cantTurnos;
-	}
+     
+ 
 
 	public int getNumeroTurno() {
  		return numeroTurno;
