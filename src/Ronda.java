8dc799d8427cefd7ff49acf5c61179810a4ad27f
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2019-09-26 17:45:04 -0300

    Update Ronda.java

--- src/Ronda.java
+++ src/Ronda.java
@@ -1,10 +1,49 @@
+import java.util.List;
 
 public class Ronda {
-int turno;
-public void inicioRonda() {}
-public boolean terminaRonda() {
-	return true;
-}
-public void inicioMiniJuego() {}
+	int turno;
+	int cantTurnos;
+	
+	
+	
+	public Ronda(int cantTurnos) {
+		super();
+		this.cantTurnos = cantTurnos;
+	}
 
-}
+	public void InicioRonda(List<Jugador> listaJugadores,int cantJugadores,Tablero tablero) {
+		this.turno=1;//Inicio el turno en 1
+		int j;//cantidad de jugadores, es para recorrer la lista de jugadores
+		Turno suTurno=null; 
+		while(this.turno<cantJugadores) {
+			suTurno=new Turno(this.turno);//inicializo un turno 
+			j=0;
+			while(!(listaJugadores.get(j).getNroTurno()==this.turno) && j<listaJugadores.size()) {//Si no estan ordenados en la lista los jugadores por turno
+				//si no encuentro al jugador con el numero de turno que corresponde 
+				j++;//sigo buscando
+			}
+			
+			this.turno=suTurno.turno(this.turno,listaJugadores.get(j),tablero);//Inicio turno	
+		}
+	}
+	
+	
+	public boolean terminaRonda(int cantJugadores) {
+		if(this.turno<=cantJugadores)
+			return false;
+	return true;
+	}
+
+	public Jugador hayGanador(int puntosObjetivo, int cantJugadores, List<Jugador> listaJugadores){
+		int i=0;
+		while(i<cantJugadores)
+			if(listaJugadores.get(i).getPuntos()==puntosObjetivo) {
+				System.out.println("GANO JUGADOR"+listaJugadores.get(i).getNombre());
+				return listaJugadores.get(i);
+			}
+		return null;
+	}
+     
+	
+}	
+
