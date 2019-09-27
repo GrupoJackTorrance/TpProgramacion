a6640bcbb1a531adda5e37ea0cd1ecd8a941bd80
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2019-09-27 11:29:37 -0300

    Update Partida.java

--- src/Partida.java
+++ src/Partida.java
@@ -1,97 +1,85 @@
-import java.util.ArrayList;
+
 import java.util.LinkedList;
 import java.util.List;
 
-
 public class Partida {
 	
+	/**************************************PARAMETROS DE PARTIDA***********************************/
 	///int estado; PARA SABER SI UNA PARTIDA SE ENCUENTRA ACTIVA O NO
-	int rondaMax;
-	int puntosObjetivo;
+	 int rondaMax; //cantidad de rondas maximas para la partida
+	 int puntosObjetivo; //cantidad de puntos para ganar
+	 Tablero tablero; //Tablero de juego
+	 List<Jugador> jugadores = new LinkedList<Jugador>();  //Lista de jugadores
+	 int turnos; //Cantidad de turnos que va haber segun los jugadores que haya 
+	/**********************************************************************************************/
 	
-	//Tablero de juego
-	 Tablero tablero;
-	 //Lista de jugadores
-	 List<Jugador> jugadores = new LinkedList<Jugador>();
-	 
-	int turnos;
-	 
+	
+	
+	/*****************************CONSTRUCTOR DE PARTIDA*******************************************/
 	 //Generamos la partida
 	 public Partida(int rondaMax,int puntosObjetivo, int cantJugadores, List<Jugador> listaJugadores){
-		 
-		 //Se empieza del turno 0
 		 turnos = cantJugadores;
-		 
-		 //Se elige la cantidad de puntos para ganar
 		 this.puntosObjetivo = puntosObjetivo;
-		 
-		 //Se elige una cantidad de rondas maximas para la partida
 		 this.rondaMax = rondaMax;
-		 
-		 //Construimos los jugadores de la partida
 		 jugadores = listaJugadores;
-		 
-		 //Construimos el tablero de partida 
-		 tablero = new Tablero();
-		 
-		 //Construimos la ronda
-		 	Ronda ronda=new Ronda(cantJugadores);
-		 	
-		 //Ponemos a todos los jugadores para empezar
-		 for(int i=0; i<jugadores.size(); i++) {
-			//ACA NO SE COMO HACER..depende como este hecho casilla
-		 }
+		 	 
 	 }
-	 
-	/* private void crearJugadores(int num_jugadores) {
-		 //inicializamos jugadores;
-		 jugadores = new ArrayList<Jugador>(num_jugadores);
-		 
-		 //Por cada uno de los jugadores 
-		 for(int i=0; i<num_jugadores; i++) {
-			 Jugador jugador = new Jugador();
-			 //metemos al jugador en la lista de jugadores de la partida
-			 jugadores.add(jugadores.size(),jugador);
-		 }
-	 }*/
-	 
-	//Este metodo es para que lo llame ronda..para que juegue el jugador
-	 //faltaria el limite del tiempo del turno
+	 /***********************************************************************************************/
 	
-
-	public void InicioPartida(Partida partida) {
-		 //Construimos la ronda
-		 	Ronda ronda=new Ronda(partida.getTurnos());
+	 
+	 
+	 /*******************************DESAROLLO DE PARTIDA******************************************/
+	public boolean InicioPartida(Partida partida) {
+		if(partida.getTurnos()==4) {
+		 	partida.tablero=partida.elegirTablero(); //Designo tablero
+		 	partida.posicionesInciales(partida.getJugadores()); //Posicion Inicial
+		 	Ronda ronda=new Ronda(partida.getTurnos()); //Creo la ronda
+		 	partida.determinarOrdenTurno(partida.getJugadores()); //Jugadores por turno
 		for(int i=0;i<rondaMax;i++) {
-			ronda.InicioRonda(partida.jugadores,partida.getTurnos(),partida.getTablero());
+			ronda.InicioRonda(partida.jugadores,partida.getTurnos(),partida.getTablero(),partida.getPuntosObjetivo());
 			i++;
 		}
-		ronda.hayGanador(partida.getPuntosObjetivo(),partida.getTurnos(),partida.getJugadores()); 
+		partida.mostrarPosicionesFinales(partida.getJugadores()); // Muestro Resultado final
+		return true;
+		}
+		return false;
 	 }
-	
-	/* 
-	private int determinarOrdenTurno() { 
-		 //Comprobamos si ya ha terminado la partida o no 
-		 //if(tiempoTerminado() ) {
-			 ///Lanzamos la excexpión de que ya hay un ganador.
-			 
-		 //}
-		 
-		 //incrementamos para pasar de turno
-		 turno++;
-		 //Si todos los jugadores tuvieron un turno, empieza la ronda de vuelta
-		 if(turno > (jugadores.size() -1) )
-			 turno = 0;
-
-		 return turno;
-	 }	 */
+	/***********************************************************************************************/
 	
 	
 	
-	public void elegirTablero() {
-		
+	/*************************FUNCIONES PARA EL DESARROLLO DE PARTIDA*******************************/
+	//Designa el orden de los turnos y tambien puntos iniciales para cada jugador equitativamente 
+	private void determinarOrdenTurno(List<Jugador> listaJugadores) { 
+		for(int i=0;i<listaJugadores.size();i++) {
+			listaJugadores.get(i).setNroTurno(i+1);
+			listaJugadores.get(i).setPuntos(10);
+		}
+	 }	 
+	
+	//Asigno tablero a la partida
+	public Tablero elegirTablero() {
+		return new Tablero();
 	}
 	
+	
+	//Al finalizar el juego muestra el Resultado final
+	public void mostrarPosicionesFinales(List<Jugador> listaJugadores) {
+		List<Jugador> jugadores=listaJugadores.get(0).OrdenarporPuntos(listaJugadores);
+		for(int i=1;i<=listaJugadores.size();i++) {
+			System.out.println("Puesto"+i+": "+jugadores.get(i-1).getNombre()+" Personaje: "+ jugadores.get(i-1).getPersonaje()+"Puntos: "+jugadores.get(i-1).getPuntos());
+		}
+	}
+	
+	//Todos empiezan de la posicion X:0 Y:0
+	public void posicionesInciales(List<Jugador> listaJugadores) {
+		for(int i=0;i<listaJugadores.size();i++) {
+			listaJugadores.get(i).setLugarTableroX(0);
+			listaJugadores.get(i).setLugarTableroY(0);
+		}
+	}
+	/***********************************************************************************************/
+		
 		//PARA MOSTRAR Y MODIFICAR 
 		public int getTurnos() {
 			return turnos;
