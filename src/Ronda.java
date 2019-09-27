5fbdd4a8a7552c214431f8f087c78a64127efebf
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2019-09-27 19:21:26 -0300

    Update Ronda.java

--- src/Ronda.java
+++ src/Ronda.java
@@ -16,8 +16,9 @@ public class Ronda {
 	public boolean InicioRonda(List<Jugador> listaJugadores,Tablero tablero,int puntosObjetivo ) throws Exception {
 		Scanner reader= new Scanner(new File("AccionesJugadores.txt"));
 		this.turno=1;//Inicio el turno en 1
-		Turno suTurno=null; 
-		while(this.turno<this.cantTurnos && GanadorporObjetivo(puntosObjetivo,this.cantTurnos,listaJugadores)==false) {
+		Turno suTurno=null;
+		int i=0;
+		while(i<this.cantTurnos  && GanadorporObjetivo(puntosObjetivo,this.cantTurnos,listaJugadores)==false) {
 			suTurno=new Turno(this.turno);//inicializo un turno 
 			//Si esta ordenado por turno la lista
 			this.turno=suTurno.turno(this.turno,listaJugadores.get(this.turno-1),tablero,reader,listaJugadores);//Inicio turno	
@@ -30,6 +31,7 @@ public class Ronda {
 			}
 			this.turno=suTurno.turno(this.turno,listaJugadores.get(j),tablero,reader);//Inicio turno
 			*/
+			i++;
 			
 		}
 		reader.close();
