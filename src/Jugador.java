f8d14f25f47dad9eb4ee7d69e5d9f7a2ec6712ac
Franco Méndez <franco.mendez@snoopconsulting.com>
2019-09-27 21:09:20 -0300

    Nueva implementacion para moverse ante una union

--- src/Jugador.java
+++ src/Jugador.java
@@ -119,9 +119,10 @@ public void restarPuntos(int puntos) {
 public boolean usarObjeto() {
 	return true;
 }
-public void elegirCaminoEnUnion() {
-	
-	
+public int elegirCaminoEnUnion(int x, int y) {
+		System.out.println("Llegaste a una union. Elegi hacia donde queres ir:");
+		System.out.println("X: "+x+" Y: "+y);
+		return x;
 }
 public Sala crearSala(int puntosObjetivo,int maxPartidas) {
 	Sala sala= new Sala(this,puntosObjetivo,maxPartidas);
