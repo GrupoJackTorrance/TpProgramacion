1b9c0e0796cd323793a65b050aeb049b0bc73e9e
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2019-09-28 01:16:08 -0300

    Update CasillaTest.java

--- src/CasillaTest.java
+++ src/CasillaTest.java
@@ -1,7 +1,8 @@
 import static org.junit.Assert.*;
 import org.junit.Assert;
+import org.junit.Test;
 public class CasillaTest {
-	
+	@Test
 	public void aplicaEfectoTest() {
 	Efecto efectoSumarPuntos= new Efecto("sumarPuntos"); //verde
 	Efecto sinEfecto= new Efecto("sinEfecto"); // blanco
@@ -11,23 +12,23 @@ public class CasillaTest {
 	Casilla casilla2= new Casilla("rojo",efectoRestarPuntos,true);
 	Casilla casilla3= new Casilla("amarillo",efectoDarObjeto,false);
 	Casilla casilla4= new Casilla("verde",efectoSumarPuntos,false);
-	Jugador jugador= new Jugador(0, "pepe",2,20,0,1,0,0, "pepe");
+	Jugador jugador= new Jugador(0, "pepe",20,1,0,1,0,0, "pepe");
 	
 	   casilla3.aplicarEfecto(jugador);
-	//veo que se haya aplicado el efecto dar Objeto al jugador 
+	   //veo que se haya aplicado el efecto dar Objeto al jugador 
 		Assert.assertEquals(1,jugador.getObjEfectos());
-		
+	
 		casilla2.aplicarEfecto(jugador);
-	//veo que se haya aplicado el efecto de restar puntos al jugador
+		//veo que se haya aplicado el efecto de restar puntos al jugador
 		Assert.assertEquals(19,jugador.getPuntos());
 	
 		casilla4.aplicarEfecto(jugador);
-	//veo que se haya aplicado el efecto de sumar puntos al jugador
-		Assert.assertEquals(21,jugador.getPuntos());
+		//veo que se haya aplicado el efecto de sumar puntos al jugador
+		Assert.assertEquals(20,jugador.getPuntos());
 		
 		casilla.aplicarEfecto(jugador);
 		//veo que no se aplique efecto , ya que la casilla es sin efecto 
-				Assert.assertEquals(20,jugador.getObjEfectos());
+		Assert.assertEquals(20,jugador.getPuntos());
 				
 		
 	}
