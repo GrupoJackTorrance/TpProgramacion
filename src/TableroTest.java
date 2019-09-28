e15fbb39bd9624020b0e8c92437bf8f96789eeb4
AdrianDiaz94 <diazmaximilianoadrian@gmail.com>
2019-09-27 23:56:20 -0300

    se aplica el efecto al jugador cuando cae en el lugar

--- src/TableroTest.java
+++ src/TableroTest.java
@@ -8,10 +8,10 @@ public class TableroTest {
 	public void test() {
 		Tablero tablero = new Tablero();
 		
-		Jugador jugador= new Jugador(0, "pepe",0, 0,1,0,0, "pepe");
+		Jugador jugador= new Jugador(0, "pepe",2, 0,1,0,0, "pepe");
 		
 		//verifico que devuelva cuanto le falta moverse
-	    assertEquals(1,tablero.avanzarJugador(jugador,5));
+	    assertEquals(2,tablero.avanzarJugador(jugador,6));
 
 		 
 		//verifico que mueve hasta la posicion correcta
@@ -21,7 +21,11 @@ public class TableroTest {
 		// verifico las opciones cuando llega a la Union
 		assertEquals(1,tablero.obtenerOpcionX(jugador));
 		assertEquals(6, tablero.obtenerOpcionY(jugador));
-		
+		jugador.elegirCaminoEnUnion(tablero.obtenerOpcionX(jugador),jugador.getLugarTableroY() );
+		// verifico que se le aplique el efecto cuando el jugador termina de moverse
+		assertEquals(0, tablero.avanzarJugador(jugador,1));
+		//veo que se haya aplicado el efecto de la casilla al jugador 
+		assertEquals(1, jugador.getObjEfectos());
 	}
 
 }
