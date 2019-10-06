f6bd58666514d31e19a219f7e031111258165bad
teso <sebastian.tesolin@yahoo.com.ar>
2019-10-06 19:34:25 -0300

    Modificacion dadoTest

--- src/DadoTest.java
+++ src/DadoTest.java
@@ -4,10 +4,9 @@ import org.junit.Test;
 public class DadoTest {
 
 	@Test
-	public void queSumaCorrectamenteLosValores() {
+	public void numeroRandomDadoTest() {
 		Dado dado1 = new Dado();
 		dado1.setRandomGenerator(new RandomGeneratorStub(4));
-
 		Assert.assertEquals(4, dado1.tirar());
 	}
 }
