5592631e106a6dd0e43f50b5ac356b5dff2dfa54
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2019-10-23 13:12:35 -0300

    Update Casilla.java

--- src/logica/Casilla.java
+++ src/logica/Casilla.java
@@ -1,22 +1,32 @@
 package logica;
 import java.awt.Color;
+import java.awt.Image;
+
+import javax.swing.*;
 
 public class Casilla {
 
 
 	private Color color; // color diferenciara el efecto? supongo que nos va a servir para la parte
-							// gr·fica
+							// gr√°fica
 
+	private Image imagen;
 	private Efecto efecto;
 	private boolean esUnion;
 
-	public Casilla(Color color, Efecto efecto, boolean esUnion) {
-
+	public Casilla(Image imagen,Color color, Efecto efecto, boolean esUnion) {
+		this.imagen=imagen; 
 		this.color = color;
 		this.efecto = efecto;
 		this.esUnion = esUnion;
 	}
 
+	
+	public Casilla(Color color, Efecto efecto, boolean esUnion) { 
+		this.color = color;
+		this.efecto = efecto;
+		this.esUnion = esUnion;
+	}
 	public void aplicarEfecto(Jugador jugador) {
 	efecto.accion(jugador);
 	}
@@ -28,5 +38,9 @@ public class Casilla {
 	public boolean getEsUnion() {
 		return esUnion;
 	}
+
+	public Image getImagen() {
+		return imagen;
+	}	
 
 }
