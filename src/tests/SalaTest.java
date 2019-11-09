0460427dd0ae500ffa71083e1c192c2917c0084a
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2019-11-09 05:24:37 -0300

    Update SalaTest.java

--- src/tests/SalaTest.java
+++ src/tests/SalaTest.java
@@ -18,7 +18,7 @@ public class SalaTest {
 	@Before
 	public void iniciar() {
 		jugador= new Jugador("pepa","pepa");
-		sala = new Sala(jugador,10,1, "nombre", 0);
+		sala = new Sala(jugador,10,1, "nombre", 2);
 		jugador2= new Jugador("pepy","pepy");
 		jugador3= new Jugador("pepo","pepo");
 		jugador4= new Jugador("pepu","pepu");
