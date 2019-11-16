b98c170e45c2dc0a8cfd5e01934685493a5eb4eb
Nicolas <nicolasmarsico90@gmail.com>
2019-11-16 15:28:58 -0300

    Update VentanaServidor.java
    modifique un poco la vista del servidor

--- src/clienteServidor/VentanaServidor.java
+++ src/clienteServidor/VentanaServidor.java
@@ -1,8 +1,10 @@
 package clienteServidor;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -10,6 +12,7 @@ import java.net.*;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -18,10 +21,12 @@ import javax.swing.JTextField;
 public class VentanaServidor extends JFrame{
 	static ServerSocket servidor;
 	PanelVentanaServidor panelServidor;
-	public VentanaServidor(ServerSocket servidor) {
+	public VentanaServidor(ServerSocket servidor) throws UnknownHostException {
 		this.servidor=servidor;
 		setLocation(10,100);
-		setSize(300, 300);
+		setSize(500, 500);
+		setBounds(100, 100, 448, 333);
+		
 		setTitle("Servidor");
 		panelServidor=new PanelVentanaServidor();
 		add(panelServidor);
@@ -46,12 +51,36 @@ public class VentanaServidor extends JFrame{
 class PanelVentanaServidor extends JPanel {
 	private JButton botonSalir;
 	private JTextField texto;
-	JPanel panelSur = new JPanel(new GridLayout(1, 2));
+	private JLabel lblServer;
+	private JLabel lblIp;
+	private JLabel lpuerto;
+	private JTextField ipText;
+	private JTextField puerText;
+	//JPanel panelSur = new JPanel(new GridLayout(1, 2));
 	
-	public PanelVentanaServidor() {
-		texto = new JTextField("Servidor prendido");
+	public PanelVentanaServidor() throws UnknownHostException {
+		texto = new JTextField(InetAddress.getLocalHost().getHostName().toString());
 		texto.setEnabled(false);
+		
+		
+		lblServer = new JLabel("SERVIDOR");
+		lblServer.setForeground(SystemColor.textHighlight);
+		lblServer.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 18));
+		
+		lblIp = new JLabel("IP");
+		lblIp.setEnabled(true);
+		
+		lpuerto = new JLabel("Puerto");
+		lpuerto.setEnabled(true);
+		
+		ipText=new JTextField("9836");
+		ipText.setEnabled(false);
+		
+		puerText=new JTextField(InetAddress.getLocalHost().getHostAddress().toString());
+		puerText.setEnabled(false);
+		
 		botonSalir = new JButton("Apagar servidor");
+		
 		botonSalir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				VentanaServidor.cerrarServidor();
@@ -59,6 +88,11 @@ class PanelVentanaServidor extends JPanel {
 		});
 		add(texto);
 		add(botonSalir);
+		add(lblServer);
+		add(lblIp);
+		add(lpuerto);
+		add(ipText);
+		add(puerText);
 	}
 	
 	public void apagar() {
@@ -68,8 +102,13 @@ class PanelVentanaServidor extends JPanel {
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		texto.setBounds(10, 10, 170, 30);
-		botonSalir.setBounds(10, 150, 180, 20);
+		texto.setBounds(50, 40, 300, 30);
+		botonSalir.setBounds(50, 200, 300, 30);
+		lblServer.setBounds(150, 10, 200, 26);
+		lblIp.setBounds(50, 100, 100, 14);
+		lpuerto.setBounds(50, 130, 100, 14);
+		ipText.setBounds(100, 120, 100, 30);
+		puerText.setBounds(100, 90, 150, 30);
 	}
 	
 }
