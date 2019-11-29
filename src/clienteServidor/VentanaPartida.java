ad17fedd2b55568bacd2e783c53370ccbbbc3e98
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2019-11-29 04:12:56 -0300

    Update VentanaPartida.java

--- src/clienteServidor/VentanaPartida.java
+++ src/clienteServidor/VentanaPartida.java
@@ -1,6 +1,7 @@
 package clienteServidor;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 import com.google.gson.Gson;
 
 import logica.Dado;
+import logica.Jugador;
 import logica.Partida;
 
 public class VentanaPartida implements Runnable {
@@ -56,13 +58,12 @@ public class VentanaPartida implements Runnable {
 	@Override
 	public void run() {
 		Gson gson= new Gson();
-
 		try {
 			DataInputStream flujoEntrada = new DataInputStream(socketServidorCliente.getInputStream());
 			while (true) {
 				String mensaje = flujoEntrada.readUTF();
-				System.out.println("se recibio un mensaje desde el servidor a la partida "+partida.getNombre());
-				PaqueteMensaje mensajeRecibido=gson.fromJson(mensaje, PaqueteMensaje.class);
+				System.out.println("se recibio un mensaje desde el servidor a la partida"+partida.getNombre());
+				PaqueteMensaje mensajeRecibido=gson.fromJson(mensaje,PaqueteMensaje.class);
 				rePintar(mensajeRecibido);
 			}
 		} catch (IOException e1) {
@@ -71,13 +72,47 @@ public class VentanaPartida implements Runnable {
 
 	}
 	
-	private void rePintar(PaqueteMensaje mensajeRecibido) {
-		if(mensajeRecibido.accion.equals("EmpiezaPartida")){
-			actualizarPartida((String) mensajeRecibido.getObj());
+	private void rePintar(PaqueteMensaje mensajeRecibido) throws IOException {
+		if(mensajeRecibido.accion.equals("EmpiezaTurno")){
+			empiezaTurno((String) mensajeRecibido.getObj());
+		}
+		if(mensajeRecibido.accion.equals("muestraTiraDado")) {
+			muestraTiraDado();
+		}
+		if(mensajeRecibido.accion.equals("muestraDado")) {
+			muestraDado((String)mensajeRecibido.getObj());
 		}
 	}
-	
-	private void actualizarPartida(String infoTurno) {		
+
+	private void muestraDado(String mensaje) throws IOException {
+		int cant=Integer.parseInt(mensaje.split(";")[0]);
+		int index=Integer.parseInt(mensaje.split(";")[1]);
+		try {
+			partida.getTablero().getVentanaTablero().getPanelTablero().mostrarTiraDado(cant);
+			int cantObjetos= partida.getJugadores().get(index).getCantidadObjetos();//OBJETOS ANTES DE MOVERSE
+			int puntosAnteriores = partida.getJugadores().get(index).getPuntos(); //PUNTOS DEL JUGADOR ANTES DE TIRAR EL DADO
+			partida.getTablero().avanzarJugador(partida.getJugadores().get(index), cant);
+			DataOutputStream flujoSalida = new DataOutputStream(socketClienteServidor.getOutputStream());
+			flujoSalida.writeUTF(Integer.toString(cantObjetos)+";"+Integer.toString(puntosAnteriores));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private void muestraTiraDado() throws IOException {
+		try {
+			int cant=partida.getTablero().getVentanaTablero().getPanelTablero().tirodado(new Jugador("NULL","NULL"));
+			DataOutputStream flujoSalida = new DataOutputStream(socketClienteServidor.getOutputStream());
+			flujoSalida.writeUTF(Integer.toString(cant));
+			System.out.println("cantidad enviada");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private void empiezaTurno(String infoTurno) {		
 		try {
 			partida.getTablero().getVentanaTablero().getPanelTablero().empiezaTurno(infoTurno);
 		} catch (InterruptedException e) {
